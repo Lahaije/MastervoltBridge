02d0b63768efc6c6cd35b954c509d9fd843006c6
Roger Lahaije <62953448+Lahaije@users.noreply.github.com>
2026-06-02 16:55:44 +0200

    chore: bump firmware version to 0.1.0-20260602-eda1cf4

--- firmware/esp32_inverter_bridge/settings.cpp
+++ firmware/esp32_inverter_bridge/settings.cpp
@@ -89,4 +89,4 @@ bool debugMode = true;
 // Firmware Version
 // =============================================================================
 // Format: <semver>-<YYYYMMDD>-<commit_short_hash>
-const char* FIRMWARE_VERSION = "0.1.0-20260602-4d7968f";
+const char* FIRMWARE_VERSION = "0.1.0-20260602-eda1cf4";
